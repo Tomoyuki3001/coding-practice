7cebf76e89b78b0f2cacb749340acb8ce2e7526e
Tomoyuki Fujii <mjr01300828@gmail.com>
2024-02-06 16:14:51 -0800

    Added the python folder

---
+++ Python/practice.py
@@ -0,0 +1,19 @@
+# age = 20
+# price = 19.95
+# first_name = "Tomo"
+# print(price)
+
+# name = input("What is your name? ")
+# print("Hello " + name)
+
+# birth_year = input("Enter your birth year: ")
+# age = 2024 - int(birth_year)
+# print(age)
+
+# first = input("Type the first number: ")
+# second = input("Type the second number: ")
+
+# print("Sum: ", float(first) + int(second))
+
+course = "Python for beginners"
+print("Python" in course)
