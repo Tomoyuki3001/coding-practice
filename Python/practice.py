805410d700e89f5fb9f0fcf946490b449dd5d374
Tomoyuki Fujii <mjr01300828@gmail.com>
2024-02-07 16:22:32 -0800

    Done with the first Python tutorial video

--- Python/practice.py
+++ Python/practice.py
@@ -15,5 +15,43 @@
 
 # print("Sum: ", float(first) + int(second))
 
-course = "Python for beginners"
-print("Python" in course)
+# course = "Python for beginners"
+# print("Python" in course)
+
+# price = 25
+# print(price > 10 and price < 30)
+
+# temperatuer = 45
+
+# if temperatuer > 30:
+#     print("It's a hot day")
+# elif temperatuer > 20:
+#     print("It'S a nice day")
+# else:
+#     print("It's cold")
+# print("Done")
+
+# weight = int(input("Weight: "))
+# unit = input("K (kg) or L (lbs)")
+
+# if unit.upper() == "K":
+#     converted = weight / 0.45
+#     print("Weight in lbs: " + str(converted))
+# else:
+#     converted = weight * 0.45
+#     print("Weight in Kg: " + str(converted))
+
+# i = 1
+
+# while i <= 10:
+#     print(i)
+#     i = i + 1
+
+# numbers = [1,2,3,4,5]
+# for item in numbers:
+#     print(item)
+
+# numbers = range(5, 10, 3)
+# for number in numbers:
+#     print(number)
+
